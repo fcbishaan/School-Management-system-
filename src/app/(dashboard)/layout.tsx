aaff21b277a82c1d961f7936938f7824b09853ff
Ishaan Vashishth <ishaan.vashishth@csdevents.com>
2024-11-09 13:41:12 +0700

    setup dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,11 @@ export default function DashboardLayout({
     children: React.ReactNode;
   }>) {
     return (
-        <div>Dashboard{children}</div>
-   
+        <div className="h-screen flex">
+          {/* Left*/}
+          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  bg-blue-300">l</div>
+          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-red-400">r</div>
+        </div>
+      
     );
   }
