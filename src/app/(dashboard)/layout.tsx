ac4b993a28f0d6487faf8f0c48952cfa55e71b59
Ishaan Vashishth <ishaan.vashishth@csdevents.com>
2024-11-10 13:40:56 +0700

    conpleted with menu component UI

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 export default function DashboardLayout({
     children,
   }: Readonly<{
@@ -6,8 +10,18 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
           {/* Left*/}
-          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  bg-blue-300">l</div>
-          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-red-400">r</div>
+          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  ">
+            <Link href="/" 
+            className="flex items-center justify center p-4 gap-2">
+              <Image src ="/logo.png" alt = "logo" width={32} height={32}></Image>
+              <span className="hidden lg:block">SMS</span>
+            </Link>
+            <Menu/>
+          </div>
+          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-green-500 overflow-scroll ">
+          <Navbar/>
+          {children}
+          </div>
         </div>
       
     );
