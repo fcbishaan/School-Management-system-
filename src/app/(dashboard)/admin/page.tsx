5c27f7e47cbe2d6960521ff78ac3e2af4cc7b745
Ishaan Vashishth <ishaan.vashishth@csdevents.com>
2024-11-10 22:39:34 +0700

    creating admin dashboard user card.

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,21 @@
+import UserCard from "@/components/UserCard"
+
+ 
  const AdminPage = () => {
     return (
-        <div className=' '>Admin Page</div>
+        <div className="p-4 flex gap-4 flex-col md:flex-row">
+            
+            <div className="w-full lg:w2/3">
+            <div className="flex gap-4 justify-between flex-wrap">
+            <UserCard type = "teacher"/>
+            <UserCard type = "student"/>
+            <UserCard type = "staff"/>
+            <UserCard type = "parent"/>
+            </div>
+            </div>
+
+            <div className="w-full kg:w-1/3">r</div>
+        </div>
     )
  }
  export default AdminPage
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({
             </Link>
             <Menu/>
           </div>
-          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-green-500 overflow-scroll ">
+          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-color-#f4f6f7  overflow-scroll ">
           <Navbar/>
           {children}
           </div>
